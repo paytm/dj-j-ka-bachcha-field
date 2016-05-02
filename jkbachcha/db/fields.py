a5d0a539a94eb457e954b5860b045291bcf33d7a
Taranjeet <reachtotj@gmail.com>
2016-05-02 12:38:33 +0530

    Db: Add model field

---
+++ jkbachcha/db/fields.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from jkbachcha.forms.widgets import JsonTextWidget
+
+
+class JsonField(models.TextField):
+    description = _("Json Field")
+
+    def formfield(self, **kwargs):
+        defaults = {}
+        defaults.update(**kwargs)
+        defaults.update({
+            'widget': JSONTextWidget
+        })
+        return super(JsonField, self).formfield(**defaults)
